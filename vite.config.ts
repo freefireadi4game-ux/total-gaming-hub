1b0fdcaa33eada12ae66dd554978e3a049af96b0
freefireadi4game-ux <freefireadi4game@gmail.com>
2026-09-13 10:19:49 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import { nitro } from "nitro/vite";
+import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), tailwindcss(), tanstackStart(), nitro(), react()],
+  tanstackStart: {
+    server: { entry: "server" },
+  },
+
+  nitro: {
+    preset: "vercel",
+  },
 });
